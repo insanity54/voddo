54cdd476dea37d99e8775055b6318edfedf7d139
Chris Grimmett <chris@grimtech.net>
2021-01-13 21:20:41 -0800

    refactor for testing

--- voddo.js
+++ voddo.js
@@ -17,17 +17,26 @@ const destinationRegex = /Destination:\s+(.+\.\S+)/;
 const fakePath = '/tmp/projektmalady 2021-01-01 fakevid.mp4';
 const isWatching = true;
 
+const download = (url) => {
+  // YoutubeDlWrap is kinda weird in that it returns an error
+  // rather than throwing the error or rejecting the promise.
+  debug(`downloading ${url}`);
+  return youtubedl.execPromise([url, '-f', 'best']);
+};
+
+const parseOutput = (output) => {
+  let r = destinationRegex.exec(output);
+  if (!r) {
+    throw new Error('destination filename could not be found in the ffmpegWrapper output');
+  }
+  return r[1];
+}
+
 const watch = (url, defaultDelay, maxDelay) => {
   defaultDelay = defaultDelay || 1000 * 15; // The starting retry delay
   maxDelay = maxDelay || 1000 * 60 * 10; // The maximum amount of time to delay beteween checking for a live stream.
 
   const ee = new EventEmitter();
-
-  const download = (url) => {
-    // YoutubeDlWrap is kinda weird in that it returns an error
-    // rather than throwing the error or rejecting the promise.
-    return youtubedl.execPromise([url, '-f', 'best']);
-  };
 
   const sleepFor = async (ms) => {
     return new Promise((resolve) => {
@@ -43,18 +52,16 @@ const watch = (url, defaultDelay, maxDelay) => {
       let res;
       try {
         res = await download(url);
-
       } catch (e) {
         if (typeof e.message === 'undefined' || !offlineRegex.test(e.message)) {
           console.error('throwing due to unhandled error')
           throw e;
-        } else {
-          console.log(`room is offline but we will try again in ${delay}ms`);
         }
       }
       debug(`response:${res}, delay:${delay}`);
       if (typeof res === 'string') {
-        ee.emit('complete', res);
+        const dest = parseOutput(res);
+        ee.emit('complete', dest);
         delay = defaultDelay;
       } else {
         delay = (delay * 2 > maxDelay) ? maxDelay : delay * 2;
@@ -71,5 +78,7 @@ const watch = (url, defaultDelay, maxDelay) => {
 
 
 module.exports = {
-  watch
+  watch,
+  download,
+  parseOutput
 }
