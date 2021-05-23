f0587e0d17cec19699103c279c29835922ee6dfe
Chris Grimmett <chris@grimtech.net>
2021-05-23 13:19:22 -0700

    remove datadir

--- voddo.sh
+++ voddo.sh
@@ -4,7 +4,6 @@
 init_delay=10          # The starting retry delay (in seconds.)
 max_delay=$((60*10))   # The maximum amount of time to delay beteween checking for a live stream.
 delay="${init_delay}"  # Ongoing delay counter. Doubles itself if there is no live stream.
-datadir="~/.local/futureporn"
 
 ## VARIABLES
 url="${1}"
@@ -15,9 +14,6 @@ ekko () {
 }
 
 init () {
-  if [ ! -d "${datadir}" ]; then
-    mkdir -p "${datadir}"
-  fi
 
   if [ -z ${url} ]; then
     ekko "First param must be a URL, but the first param was empty."
@@ -29,7 +25,6 @@ init () {
 
 
 main () {
-  cd "${datadir}"
   while :; do
 
     # Attempt to download the stream.
