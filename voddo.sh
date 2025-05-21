32d4a3da2b97bfaf750dcc1a886bd8b0e5d3a248
Christopher Joule Xenon <chris@grimtech.net>
2025-05-21 12:08:24 -0800

    remove js, incorporate yt-dlp

--- voddo.sh
+++ voddo.sh
@@ -2,57 +2,67 @@
 
 ## CONFIG
 init_delay=10          # The starting retry delay (in seconds.)
-max_delay=$((60*10))   # The maximum amount of time to delay beteween checking for a live stream.
+max_delay=$((60 * 10)) # The maximum amount of time to delay beteween checking for a live stream.
 delay="${init_delay}"  # Ongoing delay counter. Doubles itself if there is no live stream.
 
 ## VARIABLES
-url="${1}"
+channel_url="${1}"
 
 ## FUNCTIONS
-ekko () {
+ekko() {
   echo "[$(date)] - ${1}"
 }
 
-init () {
+init() {
 
-  if [ -z ${url} ]; then
+  if [ -z ${channel_url} ]; then
     ekko "First param must be a URL, but the first param was empty."
     exit 6
   fi
 
-  ekko "Attempting to download stream at URL ${url}... Press Ctrl+C to quit."
+  ekko "Attempting to download stream at URL ${channel_url}... Press Ctrl+C to quit."
 }
 
+main() {
 
-main () {
-  
   while :; do
 
-    datestamp=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
-    ffmpeg \
-      -headers "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0" \
-      -i "${url}" \
-      -c:v copy \
-      -c:a copy \
-      -movflags faststart \
-      -y \
-      -f mpegts \
-      "./${datestamp}.ts"
+    playlist_url=$(yt-dlp -g "${channel_url}")
+    if [ $? -ne 0 ]; then
+      ekko "yt-dlp failed"
+      playlist_url=""
+    fi
 
-    # Reset the delay time if yt-dlp exited with 0 error code
-    # This would suggest that the streamer ended their stream.
-    # In the case that the stream ending was temporary (so the streamer could fix a technical problem),
-    # we want yt-dlp to resume quickly afterwards to catch the stream as it continues.
-    if [ $? -eq 0 ]; then
-      delay=$init_delay;
+    if [ -n "$playlist_url" ]; then
+
+      datestamp=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
+      ffmpeg \
+        -headers "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0" \
+        -i "${playlist_url}" \
+        -c:v copy \
+        -c:a copy \
+        -movflags faststart \
+        -y \
+        -f mpegts \
+        "./recordings/${datestamp}.ts"
+
+      # Reset the delay time if yt-dlp exited with 0 error code
+      # This would suggest that the streamer ended their stream.
+      # In the case that the stream ending was temporary (so the streamer could fix a technical problem),
+      # we want yt-dlp to resume quickly afterwards to catch the stream as it continues.
+      if [ $? -eq 0 ]; then
+        delay=$init_delay
+      fi
+
     fi
 
     # Slowly increase the delay time between retries.
     # This is done to be polite to the streaming platform.
     # We wait longer and longer between tries, eventually maxing out at ${max_delay} seconds
-    if [ $(($delay*2)) -ge $max_delay ];
-      then delay=$max_delay
-      else delay=$(($delay*2));
+    if [ $(($delay * 2)) -ge $max_delay ]; then
+      delay=$max_delay
+    else
+      delay=$(($delay * 2))
     fi
 
     ekko "Retrying in ${delay} seconds..."
@@ -60,7 +70,6 @@ main () {
 
   done
 }
-
 
 init
 main
